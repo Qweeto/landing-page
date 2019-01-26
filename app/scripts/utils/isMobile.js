9a5df7bf6f639170fec3c86240c3ecbf09ca78bd
Deni Baskovsky <baskovsky@ya.ru>
2019-01-26 13:52:15 +0300

    mobirise

--- app/scripts/utils/isMobile.js
+++
@@ -1,7 +0,0 @@
-/**
- * @Url https://gist.github.com/qertis/e8f85cae788ca6102a59f75a6d47a201
- * @return {Boolean}
- */
-export const isMobile = () => {
-  return /Android|iPhone|iPad|iPod|BlackBerry|BB|Opera Mini/i.test(navigator.userAgent);
-};
