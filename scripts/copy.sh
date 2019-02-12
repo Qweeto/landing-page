2ebf8b4a56c3206327a32efd74c8fc73b4c50200
Deni Baskovsky <baskovsky@ya.ru>
2019-02-12 17:17:37 +0300

    mobirise ext

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 rsync -va ./static/ ./www/
+rsync -va ./mobirise/robots.txt ./www/
+rsync -va ./mobirise/sitemap.xml ./www/
 rsync -va ./mobirise/assets ./www/
 cp -v ./.tmp/index.html ./www/index.html
