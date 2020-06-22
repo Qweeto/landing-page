1e92a4d369349d45c812820afb749257d83cd900
Deni Baskovsky <baskovsky@ya.ru>
2020-06-22 10:40:59 +0300

    alias fix

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 
 exports.search = functions.https.onRequest((request, response) => {
-  response.redirect(`https://cse.google.com/cse?cx=013887321856262968721:y8vq-syjqom&q=${request.query.name}`);
+  const cx = '013887321856262968721:y8vq-syjqom';
+  response.redirect(`https://cse.google.com/cse?cx=${cx}&q=${request.query.name}`);
 });
