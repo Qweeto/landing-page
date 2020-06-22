90d9a416da376d455332dda6340ae791cfd910ca
Deni Baskovsky <baskovsky@ya.ru>
2020-06-22 12:24:12 +0300

    psi rm

--- functions/index.js
+++ functions/index.js
@@ -6,5 +6,6 @@ exports.search = functions.https.onRequest((request, response) => {
 });
 
 exports.suggest = functions.https.onRequest((request, response) => {
-  response.redirect(`https://suggest.yandex.ru/suggest-ff.cgi?part=${request.query.text}&amp;uil=ru&amp;v=3&amp;sn=5&amp;lr=10371&amp;yu=5519222721557041240`);
+  const yu = '5519222721557041240';
+  response.redirect(`https://suggest.yandex.ru/suggest-ff.cgi?part=${request.query.text}&amp;uil=ru&amp;v=3&amp;sn=5&amp;lr=10371&amp;yu=${yu}`);
 });
