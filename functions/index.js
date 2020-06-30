62173b2a1877df15d6dafeb3baedf3ede5f4d79f
Deni Baskovsky <baskovsky@ya.ru>
2020-07-01 00:10:04 +0300

    admin

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,28 @@
 const functions = require('firebase-functions');
 const dialogflow = require('dialogflow');
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://tewst-landing-page.firebaseio.com',
+});
+
 const serviceAccount = functions.config().service_account;
 
 const app = 'tewst-landing-page';
 const sessionClient = new dialogflow.SessionsClient({
-  credentials: serviceAccount,
+  credentials: {
+    type: serviceAccount.type,
+    project_id: serviceAccount.project_id,
+    private_key_id: serviceAccount.private_key_id,
+    private_key: serviceAccount.private_key,
+    client_email: serviceAccount.client_email,
+    client_id: serviceAccount.client_id,
+    auth_uri: serviceAccount.auth_uri,
+    token_uri: serviceAccount.token_uri,
+    auth_provider_x509_cert_url: serviceAccount.auth_provider_x509_cert_url,
+    client_x509_cert_url: serviceAccount.client_x509_cert_url,
+  },
 });
 
 exports.search = functions.https.onRequest((request, response) => {
